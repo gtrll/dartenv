009b48d9f0a1119cbfe7917bac7436da565f3c70
robotsrule <voidpointeryan@gmail.com>
2018-08-26 13:11:11 -0400

    suppress the warning of space dtype

--- gym/spaces/box.py
+++ gym/spaces/box.py
@@ -2,6 +2,7 @@ import numpy as np
 import gym
 from gym import logger
 
+
 class Box(gym.Space):
     """
     A box in R^n.
@@ -10,6 +11,7 @@ class Box(gym.Space):
     Example usage:
     self.action_space = spaces.Box(low=-10, high=10, shape=(1,))
     """
+
     def __init__(self, low=None, high=None, shape=None, dtype=None):
         """
         Two kinds of valid input:
@@ -28,22 +30,25 @@ class Box(gym.Space):
                 dtype = np.uint8
             else:
                 dtype = np.float32
-            logger.warn("gym.spaces.Box autodetected dtype as %s. Please provide explicit dtype." % dtype)
+            # logger.warn("gym.spaces.Box autodetected dtype as %s. Please provide explicit dtype." % dtype)
         self.low = low.astype(dtype)
         self.high = high.astype(dtype)
         gym.Space.__init__(self, shape, dtype)
 
     def sample(self):
         return gym.spaces.np_random.uniform(low=self.low, high=self.high + (0 if self.dtype.kind == 'f' else 1), size=self.low.shape).astype(self.dtype)
+
     def contains(self, x):
         return x.shape == self.shape and (x >= self.low).all() and (x <= self.high).all()
 
     def to_jsonable(self, sample_n):
         return np.array(sample_n).tolist()
+
     def from_jsonable(self, sample_n):
         return [np.asarray(sample) for sample in sample_n]
 
     def __repr__(self):
         return "Box" + str(self.shape)
+
     def __eq__(self, other):
         return np.allclose(self.low, other.low) and np.allclose(self.high, other.high)
